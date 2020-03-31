d9211ac9c415897cb2e59162420516b2cc71acda
Alan Nascimento <alan.silva.n@hotmail.com>
2020-03-31 03:46:58 -0300

    feat: pagination fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,18 +39,36 @@ exports.createPages = ({ graphql, actions }) => {
             }
             timeToRead
           }
+          next {
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
+          previous {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
         }
       }
     }
   `).then(result => {
     const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(({ node }) => {
+    posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
         component: resolve(`./src/templates/BlogPost/BlogPost.js`),
         context: {
           slug: node.fields.slug,
+          nextPost: previous,
+          previousPost: next,
         },
       })
     })
