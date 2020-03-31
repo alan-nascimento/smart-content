e64c2194bfdbacb7f2282ef9366fed3fac381dea
Alan Nascimento <alan.silva.n@hotmail.com>
2020-03-31 02:09:51 -0300

    feat: create node

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,9 @@ const IndexPage = () => {
       allMarkdownRemark {
         edges {
           node {
+            fields {
+              slug
+            }
             frontmatter {
               background
               category
@@ -36,12 +39,13 @@ const IndexPage = () => {
       {postList.map(
         ({
           node: {
+            fields: { slug },
             frontmatter: { background, category, date, description, title },
             timeToRead,
           },
         }) => (
           <PostItem
-            slug="/about/"
+            slug={slug}
             background={background}
             category={category}
             date={date}
